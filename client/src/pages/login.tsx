a43b654e36fedaa9bdc34d5a6cbaf7a0a66cc851
abdullahahmedmohamed@yahoo.com <abdullahahmedmohamed@yahoo.com>
2023-04-07 23:47:51 +0200

    first commit

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,67 @@
+import { useLogin } from "@refinedev/core";
+import { useEffect, useRef } from "react";
+import { Box, Container, Typography } from "@mui/material";
+import { CredentialResponse } from "../interfaces/google";
+import { yariga } from "../assets";
+
+// Todo: Update your Google Client ID here
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+console.log({ GOOGLE_CLIENT_ID });
+export const Login: React.FC = () => {
+  const { mutate: login } = useLogin<CredentialResponse>();
+  console.log("GOOGLE_CLIENT_ID", process.env.GOOGLE_CLIENT_ID);
+
+  const GoogleButton = (): JSX.Element => {
+    const divRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      if (typeof window === "undefined" || !window.google || !divRef.current) {
+        return;
+      }
+
+      try {
+        window.google.accounts.id.initialize({
+          ux_mode: "popup",
+          client_id: GOOGLE_CLIENT_ID,
+          callback: async (res: CredentialResponse) => {
+            if (res.credential) {
+              login(res);
+            }
+          },
+        });
+        window.google.accounts.id.renderButton(divRef.current, {
+          theme: "filled_blue",
+          size: "medium",
+          type: "standard",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }, []);
+
+    return <div ref={divRef} />;
+  };
+
+  return (
+    <Container
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        display="flex"
+        gap="36px"
+        justifyContent="center"
+        flexDirection="column"
+      >
+        <div style={{ margin: "auto" }}>
+          <img src={yariga} alt="Yariga Logo" />
+        </div>
+        <GoogleButton />
+      </Box>
+    </Container>
+  );
+};
